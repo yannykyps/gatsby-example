b79c38bcfb6271c760264ad92a3792e4faa80945
YannyKyprianou <yannykyps@hotmail.com>
2020-08-26 19:30:31 +0100

    fixed image issue

--- src/pages/images.js
+++ src/pages/images.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Layout from "../components/Layout"
-import Images from '../components/images'
 
 
 export default function images() {
     return (
         <Layout>
-            <Images/>
         </Layout>
     )
 }
